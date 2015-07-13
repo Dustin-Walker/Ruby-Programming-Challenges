e31666603085b8e1dbb49ae5cb4f033ba489fef7
Dustin Walker <duwalker88@gmail.com>
2015-07-13 01:23:15 -0400

    Added a feature to the ManglingSentences program that allows it to correctly handle punctuation according to the problem specifications.

--- ManglingSentences.rb
+++ ManglingSentences.rb
@@ -1,7 +1,8 @@
-# Challenge #220
-# https://www.reddit.com/r/dailyprogrammer/comments/3aqvjn/20150622_challenge_220_easy_mangling_sentences/
 # This version treats upper-case letters different from lower-case letters.
 input = gets.chomp.split
 input.each do |word|
-  print word.chars.sort.join + " "
+  word.partition(/\W/).each do |wordPart|
+    print wordPart.chars.sort.join
+  end
+  print " "
 end
