1990a80fc0e3b0855dcb591835608b92ec49dbea
Dustin Walker <duwalker88@gmail.com>
2015-07-14 17:47:50 -0400

    Completed the garland challenge to find the highest garland degree word in an English dictionary.

--- Garland.rb
+++ Garland.rb
@@ -1,14 +1,8 @@
 # From: https://www.reddit.com/r/dailyprogrammer/comments/3d4fwj/20150713_challenge_223_easy_garland_words/
 # A garland word is one that starts and ends with the same N letters in the same order, for some N greater than 0, but
-# less than the length of the word. I'll call the maximum N for which this works the garland word's degree. For
-# instance, "onion" is a garland word of degree 2, because its first 2 letters "on" are the same as its last 2 letters.
-# Examples
-# garland("programmer") -> 0
-# garland("ceramic") -> 1
-# garland("onion") -> 2
-# garland("alfalfa") -> 4
-# TODO Find the largest degree of any garland word in the enable1 English word list.
-# This method is O(log n) in time complexity
+# less than the length of the word.
+# This method is O(log n) in time complexity.
+# This method returns the value N as described above.
 def garland(input) 
   inputLength = input.length
   ((inputLength/2)+1).times do |x|
@@ -20,5 +14,20 @@ def garland(input)
   end
   0 # return here when string has garland-degree of 0
 end
-userInput = gets.chomp
-p garland(userInput).to_s
+
+# Find the largest degree of any garland word in the enable1 English word list.
+# Answer: undergrounder with a garland degree of 5
+fileName = "enable1.txt"
+dictionary = open(fileName)
+highestGarlandDegree = 4
+highestGarlandWord = "alfalfa"
+IO.foreach(fileName) do |word|
+  word.chomp!
+  if garland(word) > highestGarlandDegree
+    highestGarlandDegree = garland(word)
+    highestGarlandWord = word
+  end
+end
+
+p highestGarlandWord
+p highestGarlandDegree
