139a6004c7d71db32a77ec830339a963a5b844f2
Dustin Walker <duwalker88@gmail.com>
2015-10-20 11:04:41 -0400

    Fixed an issue with the white space on the Broken Keyboard challenge.

--- BrokenKeyboard.rb
+++ BrokenKeyboard.rb
@@ -6,17 +6,17 @@ def longestWordUsingChars(input, dictionaryFilePath)
   dict.gsub!(/\r\n?/, "\n")
   longest = Array.new
   pattern = "^[" << input << "]+$" # /^['input']+$/
-	regexp = Regexp.new(pattern)
+  regexp = Regexp.new(pattern)
   dict.each_line do |word|
     if(longest.empty? || word.chomp.size == longest.first.size)
-	    if(word.match(regexp))
-		    longest.concat(word.chomp.unpack("A*"))
+      if(word.match(regexp))
+        longest.concat(word.chomp.unpack("A*"))
       end
     end
     if(longest.empty? || word.chomp.size > longest.first.size)
-	    if(word.match(regexp))
-	      longest.clear
-		    longest.concat(word.chomp.unpack("A*"))
+      if(word.match(regexp))
+        longest.clear
+        longest.concat(word.chomp.unpack("A*"))
       end
     end
   end
